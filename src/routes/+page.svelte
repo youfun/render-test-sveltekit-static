552493331b078183ddbfcc142d88a4f7410ba25f
youfun <junezpe@gmail.com>
2023-08-07 11:00:36 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,104 @@
 <script>
-	import Counter from '$lib/Counter.svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
+<title>Claude.ai TTS </title>
+<body>
+	 <div 	style="background-color: white;" >
+	<div style="display: canter">
+		<h1>Introduce</h1>
+		<p>
+			Added text-to-speech (TTS) functionality for the AI replies on claude.ai website. Click the
+			"read" button below the conversation to invoke system TTS pronunciation.
+		</p>
+	</div>
+	<div>
+		<img class="image-container" aria-hidden="true" src="setting.png" alt=" setting" />
+		<img class="image-container" aria-hidden="true" src="claudeio.png" alt="usage" />
+	</div>
+	<div>
+		<h1>Install</h1>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+		<p style="text-decoration: line-through;">
+			Google Chrome Store :<a href="https://www.w3schools.com" target="_blank">check this</a>
+		</p>
 
-		to your new<br />SvelteKit app
-	</h1>
+		<p>
+			Download CRX: <a
+				href="https://www.dropbox.com/scl/fi/dzsm0tzjp4s40z6jcnp61/ohmpnipjdjlahinfhicglkkbppheemkf_main.crx?dl=0&rlkey=cahe4p7uvnof7q7gxb3lqw5re"
+				target="_blank">check this</a
+			>
+		</p>
 
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
+		<blockquote class="custom-blockquote">
+			<p style="font-weight: bold">Install Tutorial</p>
 
-	<Counter />
-</section>
+			<p>
+				* Open `Extensions` in the browser or enter `chrome://extensions/` in the address bar and
+				press Enter to go to the Extensions page;
+			</p>
+			<p>* Drag and drop the `crx` package into the browser;</p>
+			<p>
+				* If unable to drag and drop normally, open `Developer mode` on the `chrome://extensions/`
+				page;
+			</p>
+			<p>
+				* If the browser supports `crx` packages, it will install automatically; otherwise it will
+				report an error;
+			</p>
+			<p>* You can use it after installation is complete.</p>
+		</blockquote>
+	</div>
+	<div>
+		<h1>Privacy Policy</h1>
+
+		<p>
+			This Chrome extension does not collect, store or transmit any user data. This Chrome extension
+			is only usable on the Claude.io website. This extension fully respects user privacy and does
+			not infringe on any of the user's privacy rights. All functionalities are executed locally
+			without any internet data transmission (Note that if you choose the online voice option in the
+			voice selection, it may require internet access, but the data transmission involved is
+			irrelevant to this extension). This extension complies with the relevant regulations of the
+			Chrome Web Store, is free of charge, and does not contain any ads. If any collection of user
+			information by this extension is discovered, please contact us immediately and we will rectify
+			it in a timely manner. Protecting user privacy is our top priority. The final interpretation
+			of this privacy policy belongs to the owner of this extension. If you have any questions,
+			please feel free to contact us.
+		</p>
+		<p>Privacy Policy Update Date: August 6, 2023</p>
+	</div>
+</div>
+</body>
 
 <style>
-	section {
+	body {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex: 1;
+		background-color: gray;
+		margin: 0;
+	}
+	div { 
+		margin: 0 -5px;
+		width: 800px;
+		padding: 10px;
+	}
+	.custom-blockquote {
+		background-color: #f9f9f9;
+		border-left: 4px solid #ccc;
+		padding: 1px;
+		margin: 10px 0;
+		margin-left: 20px;
 	}
 
-	h1 {
-		width: 100%;
+	.custom-blockquote p {
+		font-size: 16px;
+		font-style: italic;
+		color: #555;
 	}
 
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.image-container {
+		max-width: 300px;
+		max-height: 200px;
 	}
 </style>
